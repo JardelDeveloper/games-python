ba5e9fa210ad1436106e61744da34e2492475535
Jardel <developerprojectfullstack@gmail.com>
2020-08-28 21:22:29 -0300

    create jogos

---
+++ jogos.py
@@ -0,0 +1,23 @@
+import forca
+import adivinhacao
+
+# modularização
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual jogo? "))
+
+    if (jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif (jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+# condição para execução do programa principal
+if (__name__ == "__main__"):
+    escolhe_jogo()
